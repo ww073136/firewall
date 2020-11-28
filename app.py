a431e916b0919d9fef513f4079b7e46bd4f35038
Andrew <awchisholm@gmail.com>
2020-11-28 16:01:07 +0000

    save

--- app.py
+++ app.py
@@ -10,10 +10,25 @@ app = Flask(__name__)
 @app.route('/')
 def summary():
   sqlite_engine = create_engine('sqlite:///pcap.db')
-  query = 'select ip_src as Sender, ip_dst as Receiver, sum(length) as TotalBytes from summary '\
-    'group by Sender, Receiver '\
+  query = ''\
+    'select ip_src as Sender_IP, ip_dst as Receiver_IP, sum(length) as TotalBytes from summary '\
+    'group by Sender_IP, Receiver_IP '\
     'order by TotalBytes desc '\
     'limit 3'
+  totalBytes = pd.read_sql(sql = query, con = sqlite_engine)
+  return totalBytes.to_html()
+  
+@app.route('/topsenders')
+def topsenders():
+  sqlite_engine = create_engine('sqlite:///capture.db')
+  query = ''\
+    'select traffic.ip_sender, dns.host_name as host_name_sender, sum(bytes) as total_bytes_sent'\
+    'from traffic'\
+    'left join dns'\
+    'ON traffic.ip_sender = dns.ip_address'\
+    'where traffic.ip = 1'\
+    'group by ip_sender'\
+    'order by total_bytes_sent desc'
   totalBytes = pd.read_sql(sql = query, con = sqlite_engine)
   return totalBytes.to_html()
 
